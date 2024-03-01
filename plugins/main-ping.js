1d26682338a2661b4db76b7b0f6e468c4dadedb4
TOXIC-ASAP <112757088+TOXIC-ASAP@users.noreply.github.com>
2024-03-01 12:27:25 +0100

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -4,7 +4,7 @@ import speed from 'performance-now'
 let handler = async (m, { conn }) => {
   let thumbnail = 'https://www.guruapi.tech/K.jpg'
   let fgg = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `OMAR-BOT`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'OMAR-BOT'\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
-  let pingMsg = await conn.sendMessage(m.chat, {text: 'جارى حساب سرعة الping...'}, {quoted: fgg})
+  let pingMsg = await conn.sendMessage(m.chat, {text: 'جارى حساب سرعة ال ping...'}, {quoted: fgg})
 
   let timestamp = speed()
 

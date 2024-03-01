1b687547b75e39e5567084d2c58ad550b2fe8d50
TOXIC-ASAP <112757088+TOXIC-ASAP@users.noreply.github.com>
2024-03-01 12:24:49 +0100

    Update tool-tts.js

--- plugins/tool-tts.js
+++ plugins/tool-tts.js
@@ -1,45 +1,62 @@
 import gtts from 'node-gtts'
-import { readFileSync, unlinkSync } from 'fs'
-import { join } from 'path'
-
+import {
+    readFileSync,
+    unlinkSync
+} from 'fs'
+import {
+    join
+} from 'path'
+import fetch from 'node-fetch'
 const defaultLang = 'id'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    command
+}) => {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom());
+    let name = await conn.getName(who);
 
-  let lang = args[0]
-  let text = args.slice(1).join(' ')
-  if ((args[0] || '').length !== 2) {
-    lang = defaultLang
-    text = args.join(' ')
-  }
-  if (!text && m.quoted?.text) text = m.quoted.text
+    let lang = args[0] || defaultLang;
+    let text = args.slice(1).join(' ') || (m.quoted?.text || '');
 
-  let res
-  try { res = await tts(text, lang) }
-  catch (e) {
-    m.reply(e + '')
-    text = args.join(' ')
-    if (!text) throw `Use example ${usedPrefix}${command} en hello world`
-    res = await tts(text, defaultLang)
-  } finally {
-    if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
-  }
+    try {
+        if (!text) throw `قم بالرد أو كتابة النص الذي تريد ترجمته \n ex:.tts ar <hello jitoss jow are u>`;
+
+        let res = await tts(text, lang);
+
+        if (res) {
+            await conn.sendFile(m.chat, res, '', '', m, null, {
+                ptt: true,
+                waveform: [100, 0, 100, 0, 100, 0, 100],
+                contextInfo: adReplyS.contextInfo
+            });
+        }
+    } catch (e) {
+        m.reply(`${e}`);
+        console.error('حدث خطأ..:', e);
+    }
+
 }
 handler.help = ['tts <lang> <teks>']
 handler.tags = ['tools']
-handler.command = /^g?tts$/i
+handler.command = /^(gtts|tts)$/i
 
 export default handler
 
 function tts(text, lang = 'id') {
-  console.log(lang, text)
-  return new Promise((resolve, reject) => {
-    try {
-      let tts = gtts(lang)
-      let filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav')
-      tts.save(filePath, text, () => {
-        resolve(readFileSync(filePath))
-        unlinkSync(filePath)
-      })
-    } catch (e) { reject(e) }
-  })
+    console.log(lang, text)
+    return new Promise((resolve, reject) => {
+        try {
+            let tts = gtts(lang)
+            let filePath = join(global.__dirname(import.meta.url), '../../tmp', (1 * new Date) + '.wav')
+            tts.save(filePath, text, () => {
+                resolve(readFileSync(filePath))
+                unlinkSync(filePath)
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
 }

654d6bf735420998a1f119b4f0029d1162dde40a
TOXIC-ASAP <112757088+TOXIC-ASAP@users.noreply.github.com>
2024-03-01 12:56:59 +0100

    Update tool-qrcode.js

--- plugins/tool-qrcode.js
+++ plugins/tool-qrcode.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
 conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', 'تابع صانع البوت فى إنستجرام \n https://www.instagram.com/ovmar_1', m)
 }
 
-handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
+handler.help = ['', 'code'].map(v => 'qr' + v + ' <نص>')
 handler.tags = ['tools']
 handler.command = /^qr(code)?$/i
 

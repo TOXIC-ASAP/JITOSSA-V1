9ee5f5b22eda3d801e59139de4ba8fadcd37ae30
TOXIC-ASAP <112757088+TOXIC-ASAP@users.noreply.github.com>
2024-03-01 12:49:43 +0100

    Update tool-qrcode.js

--- plugins/tool-qrcode.js
+++ plugins/tool-qrcode.js
@@ -1,9 +1,9 @@
 import { toDataURL } from 'qrcode'
 
 let handler = async (m, { conn, text }) => {
-    if (!text) return conn.reply(m.chat, 'Use example: \n.qrcode Clara Aubert', m)
+    if (!text) return conn.reply(m.chat, 'تحويل نص الى مربع Qrcode example: .qr hello jitoss', m)
 
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', 'تابع صانع البوت فى إنستجرام \n https://www.instagram.com/ovmar_1', m)
 }
 
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')

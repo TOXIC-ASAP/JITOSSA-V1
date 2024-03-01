8b57353fa77a09858e7109a1272ef8e0427bb97b
TOXIC-ASAP <112757088+TOXIC-ASAP@users.noreply.github.com>
2024-03-01 13:05:55 +0100

    Update sticker-toimg.js

--- plugins/sticker-toimg.js
+++ plugins/sticker-toimg.js
@@ -1,13 +1,13 @@
 import { webp2png } from '../lib/webp2mp4.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `Reply sticker with command *${usedPrefix + command}*`
+    const notStickerMessage = `قم بالرد على الملصق الذي تريد تحويله الى صورة command *${usedPrefix + command}*`
     if (!m.quoted) throw notStickerMessage
     const q = m.quoted || m
     let mime = q.mediaType || ''
     if (!/sticker/.test(mime)) throw notStickerMessage
     let media = await q.download()
     let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'out.png', '*DONE (≧ω≦)ゞ*', m)
+    await conn.sendFile(m.chat, out, 'out.png', 'ثم تحويل الملصق الى صورة \n تابع صانع البوت فى إنستجرام \n https://www.instagram.com/ovmar_1', m)
 }
 handler.help = ['toimg (reply)']
 handler.tags = ['sticker']
